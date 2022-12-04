c2ac90a8971f8c100ab758e4a1f61b0c7c70ce33
Paul Wicks <pwicks86@gmail.com>
2022-12-04 08:20:51 -0800

    day 2 complete

---
+++ day-02/p2.py
@@ -0,0 +1,41 @@
+import sys
+import os
+
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+
+move_map = {
+    ord("A"): "R",
+    ord("B"): "P",
+    ord("C"): "S",
+    ord("X"): "R",
+    ord("Y"): "P",
+    ord("Z"): "S"
+}
+
+score = 0
+for line in lines:
+    opp, me = [ord(m) for m in line.split()]
+
+    opp = opp - ord('A')
+    me = me - ord('X')
+    if me == 0:
+        me = (opp - 1) % 3
+    elif me == 1:
+        me = opp
+    elif me == 2:
+        me = (opp + 1) % 3
+
+    if me == opp:
+        score += 3 + me + 1
+        continue
+    if me == ((opp + 1) % 3):
+        # win
+        score += 6 + me + 1
+    else:
+        # loss
+        score +=  me + 1
+
+print(score)
+
+
